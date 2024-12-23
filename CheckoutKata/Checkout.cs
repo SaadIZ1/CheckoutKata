211265318af753efe61b13b8f4abdf18c0acf7f9
saad <saadiftikhar123@gmail.com>
2024-12-23 10:06:53 +0000

    Get total for one item

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -5,20 +5,29 @@ namespace CheckoutKata
     public class Checkout : ICheckout
     {
         private List<char> _items;
+        private Dictionary<char, int> skuDict;
 
         public Checkout()
         {
             _items = new List<char> { };
+            skuDict = new Dictionary<char, int> { { 'A', 50 } };
         }
 
         public int GetTotaPrice()
         {
-            return 0;
+            var totalPrice = 0;
+
+            for (int i = 0; i < _items.Count; i++)
+            {
+                totalPrice += skuDict[_items[i]];   
+            }
+
+            return totalPrice;
         }
 
         public void Scan(string item)
         {
-            if (String.IsNullOrEmpty(item))
+            if (!String.IsNullOrEmpty(item))
             {
                 _items.AddRange(item);
             }
--- CheckoutKata/CheckoutTest.cs
+++ CheckoutKata/CheckoutTest.cs
@@ -14,8 +14,16 @@ namespace CheckoutKata
             checkout.Scan(val1);
             var result = checkout.GetTotaPrice();
             result.Should().Be(0);
-
-
         }
+
+        [Theory]
+        [InlineData("A")]
+        public void GetCorrectTotalPriceFor1Items(string val1)
+        {
+            checkout.Scan(val1);
+            var result = checkout.GetTotaPrice();
+            result.Should().Be(50);
+        }
+
     }
 }
